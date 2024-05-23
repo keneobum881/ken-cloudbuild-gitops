9ae44e1fd21faccfe97f70bc4fcaf5e5f557817d
keneobum881 <akabuezeokenechukwu@gmail.com>
2024-05-22 22:49:05 -0400

    deploying gke resource

--- modules/gke/app.tf
+++ modules/gke/app.tf
@@ -101,8 +101,6 @@ resource "kubernetes_service_v1" "learning_s" {
       app = kubernetes_deployment_v1.learning.spec[0].selector[0].match_labels.app
     }
 
-    ip_family_policy = "RequireDualStack"
-
     port {
       port        = 80
       target_port = kubernetes_deployment_v1.learning.spec[0].template[0].spec[0].container[0].port[0].name
