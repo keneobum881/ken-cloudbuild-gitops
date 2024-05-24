b8a44f1312080122d85c18774c264013d3f4b031
keneobum881 <akabuezeokenechukwu@gmail.com>
2024-05-24 11:10:10 -0400

    delete

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -21,13 +21,13 @@ provider "google" {
   project = "${var.project}"
 }
 
-module "vpc" {
-  source  = "../../modules/vpc"
-  project = "${var.project}"
-  env     = "${local.env}"
-}
+# module "vpc" {
+#   source  = "../../modules/vpc"
+#   project = "${var.project}"
+#   env     = "${local.env}"
+# }
 
-module "gke_cluster" {
-  source  = "../../modules/gke"
-  project = "${var.project}"
-}
+# module "gke_cluster" {
+#   source  = "../../modules/gke"
+#   project = "${var.project}"
+# }
