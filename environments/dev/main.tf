2e9c42af57a928e85e25245acc4c46cf4b058371
keneobum881 <akabuezeokenechukwu@gmail.com>
2024-05-30 09:08:49 -0400

    updated code

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -6,11 +6,11 @@ provider "google" {
   project = "${var.project}"
 }
 
-module "vpc" {
-  source  = "../../modules/vpc"
-  project = "${var.project}"
-  env     = "${local.env}"
-}
+# module "vpc" {
+#   source  = "../../modules/vpc"
+#   project = "${var.project}"
+#   env     = "${local.env}"
+# }
 
 # module "gke_cluster" {
 #   source  = "../../modules/gke"
