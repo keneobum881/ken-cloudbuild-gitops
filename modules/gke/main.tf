a84da38ecb0b007c687ebedf075be93e94c9e8db
keneobum881 <akabuezeokenechukwu@gmail.com>
2024-06-05 14:45:06 -0400

    deploying gke

--- modules/gke/main.tf
+++ modules/gke/main.tf
@@ -10,20 +10,21 @@ resource "google_container_cluster" "learning" {
   subnetwork = "${local.subnet}"
   remove_default_node_pool = true
   initial_node_count       = 1
+  deletion_protection = false
 }
 
-resource "google_container_node_pool" "learning_preemptible_nodes" {
-  name       = "${var.gke_name}-node-pool"
-  location   = "${var.region}"
-  cluster    = google_container_cluster.learning.name
-  node_count = 1
+# resource "google_container_node_pool" "learning_preemptible_nodes" {
+#   name       = "${var.gke_name}-node-pool"
+#   location   = "${var.region}"
+#   cluster    = google_container_cluster.learning.name
+#   node_count = 1
 
-  node_config {
-    preemptible  = true
-    machine_type = "e2-micro"
-    service_account = "id-420039265868-ken@aksgkelearning.iam.gserviceaccount.com"
-    oauth_scopes    = [
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
-  }
-}
+#   node_config {
+#     preemptible  = true
+#     machine_type = "e2-micro"
+#     service_account = "id-420039265868-ken@aksgkelearning.iam.gserviceaccount.com"
+#     oauth_scopes    = [
+#       "https://www.googleapis.com/auth/cloud-platform"
+#     ]
+#   }
+# }
